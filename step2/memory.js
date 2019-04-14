f4a3bf8b60fe639f719e6cbcf5d87fa2a7c19901
janghyoseog <bestdevhyo1225@gmail.com>
2019-04-15 01:27:00 +0900

    Modify the Memory Module

--- step2/memory.js
+++ step2/memory.js
@@ -1,4 +1,3 @@
-const memorySpace       = [ new Array(1 << 16), new Uint16Array(1 << 16)];
 const MEMORY_TEXT_START = 0x0000;            // 0
 const MEMORY_TEXT_END   = 0xFFFF;            // 65535
 const MEMORY_HEAP_START = 0x10000;           // 65536
@@ -7,65 +6,71 @@ const TEXT_AREA         = 0;
 const HEAP_AREA         = 1;
 const TOTAL_AREA        = 3;
 
-const checkRange = (area, property) => {
-    return property.every((address) => { 
-        switch(area) {
-            case TOTAL_AREA : return MEMORY_TEXT_START <= address && address <= MEMORY_HEAP_END; 
-            case TEXT_AREA  : return MEMORY_TEXT_START <= address && address <= MEMORY_TEXT_END;
-            case HEAP_AREA  : return MEMORY_HEAP_START <= address && address <= MEMORY_HEAP_END;
-        }
-    });
-}
+class Memory {
+    constructor(memorySpace) {
+        this.memorySpace = memorySpace;
+        this.textAreaCounter = 0x0000;
+    }
 
-const isEmptyBuffer = (area, address) => { return memorySpace[area][address] === undefined; }
+    checkRange(area, property) {
+        return property.every((address) => { 
+            switch(area) {
+                case TOTAL_AREA : return MEMORY_TEXT_START <= address && address <= MEMORY_HEAP_END; 
+                case TEXT_AREA  : return MEMORY_TEXT_START <= address && address <= MEMORY_TEXT_END;
+                case HEAP_AREA  : return MEMORY_HEAP_START <= address && address <= MEMORY_HEAP_END;
+            }
+        });
+    }
 
-module.exports = {    
-    peek : (int32_address) => {
-        if (!checkRange(TOTAL_AREA, [ int32_address ]))
+    /*isEmpty(address) { 
+        return this.memorySpace[address] !== undefined; 
+    }*/
+
+    peek(int32_address) {
+        if (!this.checkRange(TOTAL_AREA, [ int32_address ]))
             throw new Error(`Out of Range Exception!`);
 
-        if (int32_address < 0x10000) 
-            return memorySpace[TEXT_AREA][int32_address];
-        else 
-            return memorySpace[HEAP_AREA][MEMORY_HEAP_END - int32_address];
-    },
-    
-    locate : (int16_program) => {
+        return this.memorySpace[int32_address];
+    }
+
+    locate(int16_program) {
         const start_address = 0;
         const end_address = int16_program.length - 1;
         
-        if (!checkRange(TEXT_AREA, [ start_address, end_address ])) 
+        if (!this.checkRange(TEXT_AREA, [ start_address, end_address ])) 
             throw new Error(`Out of Range Exception Program Address!`);
 
-        for (let address = 0; address < int16_program.length; address+=2) {
-            if (!isEmptyBuffer(TEXT_AREA, address)) 
-                throw new Error(`A value exists at the current memory address!`);
-        }
-
-        memorySpace[TEXT_AREA] = int16_program;
-    },
-    
-    fetch : (int16_programCounter) => {
-        if (!checkRange(TEXT_AREA, [ int16_programCounter ]))
-            throw new Error(`Out of Range Exception!`);
-
-        return memorySpace[TEXT_AREA][int16_programCounter];
-    },
-    
-    load : (int16_address) => {
-        if (!checkRange(HEAP_AREA, [ int16_address ])) 
-            throw new Error(`Out of Range Exception!`);
-
-        return memorySpace[HEAP_AREA][MEMORY_HEAP_END - int16_address];
-    },
-    
-    store : (int16_address, int16_data) => {
-        if (!checkRange(HEAP_AREA, [ int16_address ])) 
-            throw new Error(`Out of Range Exception!`);
+        int16_program.forEach( IR => {
+            /*if (!this.isEmpty(this.textAreaCount)) 
+                throw new Error(`A value exists at the current memory address!`);*/
         
-        if (!isEmptyBuffer(HEAP_AREA, int16_address)) 
-            throw new Error(`A value exists at the current memory address!`);
-        
-        memorySpace[HEAP_AREA][MEMORY_HEAP_END - int16_address] = int16_data;
-    },
+            this.memorySpace[this.textAreaCounter++] = IR;
+        } );
+    }
+
+    fetch(int16_programCounter) {
+        if (!this.checkRange(TEXT_AREA, [ int16_programCounter ]))
+            throw new Error(`Out of Range Exception!`);
+
+        return this.memorySpace[int16_programCounter];
+    }
+    
+    load(int16_address) {
+        if (!this.checkRange(HEAP_AREA, [ MEMORY_HEAP_START + int16_address ])) 
+            throw new Error(`Out of Range Exception!`);
+
+        return this.memorySpace[MEMORY_HEAP_START + int16_address];
+    }
+    
+    store(int16_address, int16_data) {
+        if (!this.checkRange(HEAP_AREA, [ MEMORY_HEAP_START + int16_address ])) 
+            throw new Error(`Out of Range Exception!`);
+
+        /*if (!this.isEmpty(int16_address)) 
+            throw new Error(`A value exists at the current memory address!`);*/
+
+        this.memorySpace[MEMORY_HEAP_START + int16_address] = int16_data;
+    }
 }
+
+module.exports = Memory;
